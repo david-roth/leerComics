ede65c0f53867e81b65a5f5e3b13b1f2729cba08
David Roth <david.roth.1987>
2014-09-27 22:15:35 -0300

    first template of an arc comic getting from a db

--- index.php
+++ index.php
@@ -7,14 +7,19 @@ if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-
-$result = mysqli_query($con,"SELECT * FROM comic");
-
-$rows = array();
-while($r = mysqli_fetch_assoc($result)) {
-    $rows[] = $r;
+//set charset to utf8
+if (!$con->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $con->error);
+} else {
+    //get the comics
+    $result = mysqli_query($con,"SELECT * FROM comic");
+    $rows = array();
+    while($r = mysqli_fetch_assoc($result)) {
+        $rows[] = $r;
+    }
+    //transform to json
+    print json_encode($rows);
 }
-print json_encode($rows);
 
-
+//close connection
 mysqli_close($con);
